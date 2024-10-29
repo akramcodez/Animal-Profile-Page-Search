f082bb2ccfe24c5114b7bbd042d4adf843335055
akramcodez <skcodewizard786@gmail.com>
2024-10-29 08:53:24 +0530

    modified files after contribution

---
+++ public/js/home.js
@@ -0,0 +1,20 @@
+let profileImg = document.querySelector("#profileImg");
+let mainImg = document.querySelector("#mainImg");
+
+let url = "https://dog.ceo/api/breeds/image/random";
+let url2 = "https://randomfox.ca/floof/?ref=public_apis";
+
+async function getImage() {
+    try {
+        let res = await axios.get(url);
+        let res2 = await axios.get(url2);
+        profileImg.src = res.data.message;
+        mainImg.src = res2.data.image;
+    } catch(e) {
+        console.log("failed loading profile image...");
+    }
+}
+
+window.onload = function() {
+    getImage();
+};
