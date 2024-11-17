7200ddfa083a230db59064fd0a8254c8a5a8b015
akramcodez <skcodewizard786@gmail.com>
2024-11-17 10:38:39 +0530

    Improved folder structure

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // Importing Modules and Setting Up Constants
-const express = require("express");
-const session = require("express-session");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-const path = require("path");
-const {readData,
-writeData,
-readPosts,
-readExplorePosts,
-readMessages,
-messageFilePath
-} = require("./utils/filePaths")
+const express = require('express');
+const session = require('express-session');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const {
+  readData,
+  writeData,
+  readPosts,
+  readExplorePosts,
+  readMessages,
+  messageFilePath,
+} = require('./utils/filePaths');
 
 // Import auto-replies from external file
-const autoReplies = require("./config/autoReplies");
+const autoReplies = require('./config/autoReplies');
 
 const app = express();
 const port = 8080;
@@ -21,138 +22,93 @@ const port = 8080;
 // Middleware and App Configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname, "/public")));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Set up session middleware
 app.use(
   session({
-    secret: "your_secret_key",
+    secret: 'your_secret_key',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
-  })
+  }),
 );
 
 // Load data from index.json
 let data = readData();
 
-// Check and update post IDs if needed in index.json
-let needsUpdate = false;
-for (const key in data) {
-  const user = data[key];
-  user.posts = user.posts.map((post) => {
-    if (post.id === "no") {
-      needsUpdate = true;
-      return {
-        ...post,
-        id: uuidv4(),
-      };
-    }
-    return post;
-  });
-}
-
-// Only write data if updates were made in index.json
-if (needsUpdate) {
-  writeData(data);
-  console.log("IDs updated in index.json");
-} else {
-  console.log("No IDs needed updating in index.json");
-}
-
 // Load data from explore.json
 let exploreData = readExplorePosts();
-
-// Check and update post IDs if needed in explore.json
-let needsUpdate2 = false;
-exploreData.forEach((entry) => {
-  entry.pic = entry.pic.map((post) => {
-    if (post.id === "no") {
-      needsUpdate2 = true;
-      return {
-        ...post,
-        id: uuidv4(),
-      };
-    }
-    return post;
-  });
-});
-
-// Only write data if updates were made in explore.json
-if (needsUpdate2) {
-  writeExplorePosts(exploreData);
-  console.log("IDs updated in explore.json");
-} else {
-  console.log("No IDs needed updating in explore.json");
-}
 
 // Authentication Middleware
 const authMiddleware = (req, res, next) => {
   if (!req.session.isAuthenticated) {
-    return res.redirect("/");
+    return res.redirect('/');
   }
   next();
 };
 
 // Route: Sign-up / Sign-in Page
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   if (req.session.isAuthenticated) {
-    return res.redirect("/ig");
+    return res.redirect('/ig');
   }
-  res.render("auth-ejs/signup.ejs", { error: "" });
+  res.render('auth-ejs/signup.ejs', { error: '' });
 });
 
 // Route: Handle Sign-up or Sign-in Logic
-app.post("/signin", (req, res) => {
+app.post('/signin', (req, res) => {
   const { username, password } = req.body;
 
-  if (username === "anigram@gmail.com" && password === "by-akram") {
+  if (username === 'anigram@gmail.com' && password === 'by-akram') {
     req.session.isAuthenticated = true;
-    res.redirect("/ig");
+    res.redirect('/ig');
   } else {
-    res.render("auth-ejs/signup.ejs", {
+    res.render('auth-ejs/signup.ejs', {
       error:
-        "Warning: Invalid Credentials! Please check the input fields, then try again",
+        'Warning: Invalid Credentials! Please check the input fields, then try again',
     });
   }
 });
 
 // Apply the authMiddleware to both /ig and /search routes
-app.use("/ig", authMiddleware);
+app.use('/ig', authMiddleware);
 
 // Route: Home Page
-app.get("/ig", (req, res) => {
+app.get('/ig', (req, res) => {
   let posts = readPosts();
   const shuffledPosts = posts.sort(() => Math.random() - 0.5);
-  res.render("main-ejs/home.ejs", { data, posts: shuffledPosts });
+  res.render('main-ejs/home.ejs', { data, posts: shuffledPosts });
 });
 
 // Route : Settings and Subpages
-app.get("/ig/settings", (req, res) => res.render("settings-ejs/setting.ejs"));
-app.get("/ig/settings/learn-more", (req, res) =>
-  res.render("settings-ejs/setting_learn_more.ejs")
+app.get('/ig/settings', (req, res) => res.render('settings-ejs/setting.ejs'));
+app.get('/ig/settings/learn-more', (req, res) =>
+  res.render('settings-ejs/setting_learn_more.ejs'),
 );
-app.get("/ig/settings/creators", (req, res) =>
-  res.render("settings-ejs/setting_creators.ejs")
+app.get('/ig/settings/creators', (req, res) =>
+  res.render('settings-ejs/setting_creators.ejs'),
 );
-app.get("/ig/settings/help", (req, res) => res.render("settings-ejs/setting_help.ejs"));
+app.get('/ig/settings/help', (req, res) =>
+  res.render('settings-ejs/setting_help.ejs'),
+);
 
 // Route : Log-out
-app.get("/log-out", (req, res) => {
+app.get('/log-out', (req, res) => {
   req.session.destroy(() => {
-    res.redirect("/");
+    res.redirect('/');
   });
 });
 
 // Route: Search Page
-app.get("/ig/search", (req, res) => {
-  res.render("main-ejs/search.ejs");
+app.get('/ig/search', (req, res) => {
+  res.render('main-ejs/search.ejs');
 });
 
 // Route: User Search Results
-app.get("/ig/search/users", (req, res) => {
+app.get('/ig/search/users', (req, res) => {
   const searchQuery = req.query.query;
   let data = readData();
   const matchedUsers = Object.keys(data)
@@ -169,7 +125,7 @@ app.get("/ig/search/users", (req, res) => {
 });
 
 // Route : Explore
-app.get("/ig/explore", (req, res) => {
+app.get('/ig/explore', (req, res) => {
   const allImages = [];
   exploreData.forEach((animal) => {
     animal.pic.forEach((image) => {
@@ -180,11 +136,11 @@ app.get("/ig/explore", (req, res) => {
   const shuffledImages = allImages.sort(() => 0.5 - Math.random());
   const randomImages = shuffledImages.slice(0, 21);
 
-  res.render("explore-ejs/explore.ejs", { images: randomImages });
+  res.render('explore-ejs/explore.ejs', { images: randomImages });
 });
 
 // Route: Show and Edit Post Pages
-app.get("/ig/explore/:id", (req, res) => {
+app.get('/ig/explore/:id', (req, res) => {
   let { id } = req.params;
   let img = null;
   let parent = null;
@@ -199,37 +155,37 @@ app.get("/ig/explore/:id", (req, res) => {
   });
 
   if (img && parent) {
-    res.render("explore-ejs/explore-zoom.ejs", { img, parent });
+    res.render('explore-ejs/explore-zoom.ejs', { img, parent });
   } else {
-    res.status(404).render("error.ejs", { message: "Post not found" });
+    res.status(404).render('error.ejs', { message: 'Post not found' });
   }
 });
 
 // Route: Animal Pages
-app.get("/ig/:username", (req, res) => {
+app.get('/ig/:username', (req, res) => {
   let instaData = readData();
   let { username } = req.params;
   let data = instaData[username.toLowerCase()];
 
-  if (username === "tiger247") {
-    res.render("profile-ejs/myProfile.ejs", { data });
+  if (username === 'tiger247') {
+    res.render('profile-ejs/myProfile.ejs', { data });
   } else if (data) {
-    res.render("posts-ejs/insta.ejs", { data });
+    res.render('posts-ejs/insta.ejs', { data });
   } else {
-    res.status(404).render("error.ejs", { message: "User not found" });
+    res.status(404).render('error.ejs', { message: 'User not found' });
   }
 });
 
 // Route: Profile Editing Page
-app.get("/ig/:username/edit-profile", (req, res) => {
+app.get('/ig/:username/edit-profile', (req, res) => {
   let instaData = readData();
   let { username } = req.params;
   let data = instaData[username];
-  res.render("profile-ejs/edit-profile.ejs", { data });
+  res.render('profile-ejs/edit-profile.ejs', { data });
 });
 
 // Route: Save Edited Profile
-app.post("/ig/:username", (req, res) => {
+app.post('/ig/:username', (req, res) => {
   let { username } = req.params;
   let instaData = readData();
   let data = instaData[username];
@@ -244,15 +200,15 @@ app.post("/ig/:username", (req, res) => {
     writeData(instaData);
 
     req.session.destroy(() => {
-      res.redirect("/");
+      res.redirect('/');
     });
   } else {
-    res.status(404).render("error.ejs", { message: "User not found" });
+    res.status(404).render('error.ejs', { message: 'User not found' });
   }
 });
 
 // Route: Delete Post
-app.post("/ig/:username/delete", (req, res) => {
+app.post('/ig/:username/delete', (req, res) => {
   const { username } = req.params;
   let users = readData();
   let user = users[username];
@@ -263,17 +219,17 @@ app.post("/ig/:username/delete", (req, res) => {
   }
 
   req.session.destroy(() => {
-    res.redirect("/");
+    res.redirect('/');
   });
 });
 
 // Route : New Page and Post Addition
-app.get("/ig/post/new", (req, res) => res.render("profile-ejs/new.ejs"));
+app.get('/ig/post/new', (req, res) => res.render('profile-ejs/new.ejs'));
 
-app.post("/ig/post/new", (req, res) => {
+app.post('/ig/post/new', (req, res) => {
   const { image } = req.body;
   let users = readData();
-  const tigerUser = users["tiger247"];
+  const tigerUser = users['tiger247'];
 
   if (tigerUser) {
     const newPost = {
@@ -288,15 +244,15 @@ app.post("/ig/post/new", (req, res) => {
     writeData(users);
 
     req.session.destroy(() => {
-      res.redirect("/");
+      res.redirect('/');
     });
   } else {
-    res.status(404).render("error.ejs", { message: "User not found" });
+    res.status(404).render('error.ejs', { message: 'User not found' });
   }
 });
 
 // Route: Show and Edit Post Pages
-app.get("/ig/posts/:id", (req, res) => {
+app.get('/ig/posts/:id', (req, res) => {
   let { id } = req.params;
   let post = null;
   let parent = null;
@@ -312,13 +268,13 @@ app.get("/ig/posts/:id", (req, res) => {
   }
 
   if (post && parent) {
-    res.render("posts-ejs/post.ejs", { post, parent });
+    res.render('posts-ejs/post.ejs', { post, parent });
   } else {
-    res.status(404).render("error.ejs", { message: "Post not found" });
+    res.status(404).render('error.ejs', { message: 'Post not found' });
   }
 });
 
-app.get("/ig/post/:id/edit", (req, res) => {
+app.get('/ig/post/:id/edit', (req, res) => {
   let { id } = req.params;
   let post = null;
 
@@ -332,13 +288,13 @@ app.get("/ig/post/:id/edit", (req, res) => {
   }
 
   if (post) {
-    res.render("profile-ejs/edit.ejs", { post });
+    res.render('profile-ejs/edit.ejs', { post });
   } else {
-    res.status(404).render("error.ejs", { message: "Post not found" });
+    res.status(404).render('error.ejs', { message: 'Post not found' });
   }
 });
 
-app.post("/ig/post/:id/edit", (req, res) => {
+app.post('/ig/post/:id/edit', (req, res) => {
   let { id } = req.params;
   let { image } = req.body;
   let post = null;
@@ -358,19 +314,19 @@ app.post("/ig/post/:id/edit", (req, res) => {
     writeData(data);
 
     req.session.destroy(() => {
-      res.redirect("/");
+      res.redirect('/');
     });
   } else {
-    res.status(404).render("error.ejs");
+    res.status(404).render('error.ejs');
   }
 });
 
 //message button clicked
-app.get("/ig/message/:user", (req, res) => {
-  res.render("message-ejs/message.ejs");
+app.get('/ig/message/:user', (req, res) => {
+  res.render('message-ejs/message.ejs');
 });
 
-app.get("/messages/:user1/:user2", (req, res) => {
+app.get('/messages/:user1/:user2', (req, res) => {
   const { user1, user2 } = req.params;
   let messages = readMessages();
   let chat = (messages[user1] && messages[user1][user2]) || [];
@@ -390,18 +346,17 @@ app.get("/messages/:user1/:user2", (req, res) => {
   res.json({ messages: chat, detail: matchedUsers });
 });
 
-
-app.post("/messages/:sender/:receiver", (req, res) => {
+app.post('/messages/:sender/:receiver', (req, res) => {
   const { sender, receiver } = req.params;
   const { message } = req.body;
   const lowerCaseMessage = message.toLowerCase();
   const timestamp = new Date().toISOString();
 
   // Load current data from message.json
-  fs.readFile(messageFilePath, "utf8", (err, data) => {
+  fs.readFile(messageFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).send("Error reading message data");
+      return res.status(500).send('Error reading message data');
     }
 
     const messages = JSON.parse(data);
@@ -436,14 +391,14 @@ app.post("/messages/:sender/:receiver", (req, res) => {
 });
 
 // Catch-all route for undefined paths under /ig
-app.use("/ig/*", (req, res) => {
-  res.status(404).render("error.ejs", { message: "Page not found" });
+app.use('/ig/*', (req, res) => {
+  res.status(404).render('error.ejs', { message: 'Page not found' });
 });
 
 // Error-handling middleware for the /ig route
-app.use("/ig", (err, req, res, next) => {
+app.use('/ig', (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).render("error.ejs", { message: "Something went wrong!" });
+  res.status(500).render('error.ejs', { message: 'Something went wrong!' });
 });
 
 // Starting the Server
